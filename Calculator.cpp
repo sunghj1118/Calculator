8ad6a7b7ae04d05a7719355815384e2764394181
Hyunjoon Sung <sunghj1118@gmail.com>
2022-12-16 16:04:47 +0900

    Added loop to menu until exiting.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,17 +1,60 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+// Addition of two numbers
+// Difference between two numbers
+// Product of two numbers
+// Division of two numbers
+
+// menu
+void menu() {
+    cout << "Press 1 to calculate sum.\n";
+    cout << "Press 2 to calculate difference.\n";
+    cout << "Press 3 to calculate product.\n";
+    cout << "Press 4 to calculate division.\n";
+    cout << "Press 5 to exit.\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void result(int choice, int a, int b) {
+    
+    switch (choice) {
+    case 1: {
+        cout << "Sum is " << (a+b) << endl;
+        break;
+    }
+    case 2: {
+        cout << "Difference is " << (a-b) << endl;
+        break;
+    }
+    case 3: {
+        cout << "Product is " << (a*b) << endl;
+        break;
+    }
+    case 4: {
+        cout << "Division is " << (a/b) << endl;
+        break;
+    }
+    case 5: {
+        cout << "Exiting.\n";
+        break;
+    }
+    default: {
+        printf("Wrong input.\n");
+        break;
+    }
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+    int a, b, choice;
+    choice = 0;
+    while (choice != 5) {
+        cout << "What are your two numbers?" << endl;
+        cin >> a >> b;
+        menu();
+        cin >> choice;
+    }
+    result(choice, a, b);
+}
